3f310ca692e2cc459fca542591b2e5a9e5be7052
Mike Wu <mikejcwu@gmail.com>
2019-02-17 13:03:17 -0800

    add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -223,26 +223,80 @@ describe('POST /users', ()=>{
       });
   })
 
-    it('should return validation errors if request invalid', (done)=>{
-      const email='example';
-      const password='';
+  it('should return validation errors if request invalid', (done)=>{
+    const email='example';
+    const password='';
 
-      request(app)
-        .post('/users')
-        .send({email, password})
-        .expect(400)
-        .end(done);
-    })
+    request(app)
+      .post('/users')
+      .send({email, password})
+      .expect(400)
+      .end(done);
+  })
 
-    it('should not create user if email in use', (done)=>{
-      const email='example@gmail.com';
-      const password='123';
+  it('should not create user if email in use', (done)=>{
+    const email='example@gmail.com';
+    const password='123';
 
-      request(app)
-        .post('/users')
-        .send({email, password})
-        .expect(400)
-        .end(done);
-    })
-
+    request(app)
+      .post('/users')
+      .send({email, password})
+      .expect(400)
+      .end(done);
+  })
 })
+
+describe('POST /users/login', ()=>{
+  it('should login user and return auth token', (done)=>{
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res)=>{
+        expect(res.headers['x-auth']).toExclude();
+      })
+      .end((err, res)=>{
+        if(err){
+          return done(err);
+        }
+
+      User.findById(users[1]._id).then((user)=>{
+        expect(user.tokens[0]).toInclude({
+          access: 'auth',
+          token: res.headers['x-auth']
+        });
+        done();
+      }).catch((e)=>{
+        done(e);
+      });
+    });
+  });
+
+  it('should reject invalid login', (done)=>{
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: ''
+      })
+      .expect(400)
+      .expect((res)=>{
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res)=>{
+        if(err){
+          return done(err);
+        }
+
+      User.findById(users[1]._id).then((user)=>{
+        expect(user.tokens.length).toBe(0);
+        done();
+      }).catch((e)=>{
+        done(e);
+      });
+    });
+  })
+});
