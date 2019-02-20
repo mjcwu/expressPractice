51a8f028c927238b5d04c3425849c9baf63e1fce
Mike Wu <mikejcwu@gmail.com>
2019-02-19 16:01:44 -0800

    update commit to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,25 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if(env === 'development' || env === 'test'){
+  var config = require('./config.json');
+  // use variables of a properties use []
+  // this return { PORT: 3000, MONGODB_URL: 'mongodb://localhost:27017/TodoApp' }
+  var envConfig = config[env]
+
+  // this return the key of variable in array
+  // return [ 'PORT', 'MONGODB_URL' ]
+  // console.log(Object.keys(envConfig));
+  Object.keys(envConfig).forEach((key)=>{
+    // process.env gives key value pairs of environmental variables
+    // following code change the environmental value of a key PORT and adding mongodb_url: value pair
+    process.env[key]=envConfig[key];
+  })
 }
+
+// if (env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
--- server/models/user.js
+++ server/models/user.js
@@ -70,7 +70,7 @@ UserSchema.methods.generateAuthToken = function (){
   // create hashing for token
   const user = this;
   const access = 'auth';
-  const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  const token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
   // store it
   // token is concat as a result, it's an array of tokens stored
@@ -104,7 +104,7 @@ UserSchema.statics.findByToken = function (token) {
   var decoded;
 
   try{
-    decoded = jwt.verify(token, 'abc123')
+    decoded = jwt.verify(token, process.env.JWT_SECRET)
   } catch (e){
     // return new Promise((response, reject)=>{
     //   reject();
